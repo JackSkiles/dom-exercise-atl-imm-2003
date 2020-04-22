34b970cdb847387fb7d01b17a71987a387b1c753
Jack Skiles <jackskiles420@gmail.com>
2020-04-22 11:23:13 -0400

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,53 @@
 const addresses = [
     "https://google.com",
     "https://bing.com",
-    "https://duckduckgo.com"
+    "https://duckduckgo.com",
+    "https://yahoo.com",
+    "https://askjeeves.com",
+    "https://lycos.com"
+
 ];
 
+const list = document.createElement("ul");
+const listContainer = document.querySelector('nav.js-link-container');
+
+listContainer.appendChild(list);
+
+
+function createLink(address) {
+    // const address = array[index];
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', address);
+    link.textContent = address;
+    listItem.appendChild(link);
+    list.appendChild(listItem);
+}
+
+
+// for (let index = 0; index < addresses.length; index++) {
+//     const address = addresses[index];
+//     createLink(address);
+// }
+  
+    
+    
+//     const address = addresses[index];
+//     const listItem = document.createElement('li');
+//     const link = document.createElement('a');
+//     link.setAttribute('href', address);
+//     link.textContent = address;
+//     listItem.appendChild(link);
+//     list.appendChild(listItem);
+// }
+
+// addresses.forEach(function (address) {
+//     const listItem = document.createElement('li');
+//     const link = document.createElement('a');
+//     link.setAttribute('href', address);
+//     link.textContent = address;
+//     listItem.appendChild(link);
+//     list.appendChild(listItem);
+// })
+
+addresses.forEach(createLink);
