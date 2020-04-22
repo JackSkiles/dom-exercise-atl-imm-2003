03543e0cb10547530d3bbf58f92050fe1702ec36
Jack Skiles <jackskiles420@gmail.com>
2020-04-22 11:56:22 -0400

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -10,19 +10,31 @@ const addresses = [
 
 ];
 
+const head = document.createElement('header');
+head.style.display = 'flex';
+head.style.justifyContent = 'center';
+head.style.alignItems = 'center';
+head.style.fontSize = "30px";
+head.style.backgroundColor = '#ccc';
+head.style.height = "10vh";
+head.textContent = 'Welcome to the search engine picker!'
+
 const div1 = document.createElement("div");
 div1.style.display = 'flex';
 div1.style.justifyContent = 'center';
+div1.style.backgroundColor = '#50A6EB'
 
 const list = document.createElement("ul");
 list.style.display = 'flex';
+list.style.justifyContent = 'center';
 // list.style.flexDirection = 'column';
 list.style.listStyleType = "none";
-list.style.width = '50%';
+list.style.fontSize = '20px'
 div1.appendChild(list);
 
 
 const listContainer = document.querySelector('nav.js-link-container');
+listContainer.appendChild(head);
 listContainer.appendChild(div1);
 
 
@@ -34,8 +46,9 @@ function createLink(address) {
     link.setAttribute('href', address);
     link.textContent = address;
     link.style.display = "inline-block";
+    link.style.color = "white";
     link.style.textDecoration = "none";
-    link.style.padding = "20px 40px 20px 0";
+    link.style.margin = "20px";
     listItem.appendChild(link);
     list.appendChild(listItem);
 }
